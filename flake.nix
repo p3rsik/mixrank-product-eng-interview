c7edbf38de3f2632fb6d2a2b8b0936cadbe70e9d
p3rsik <yroslav541+git@gmail.com>
2026-04-18 15:55:02 +0300

    feat: modernize nix setup

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        # Import your legacy default.nix and pass pkgs
+        shell = import ./default.nix;
+      in
+      {
+        devShell = shell;
+      }
+    );
+}   
